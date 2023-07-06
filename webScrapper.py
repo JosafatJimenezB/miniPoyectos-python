d235e5ed70bcab7f87fb1f26aa57b7e07e8ce2d0
josafat <josa221001@gmail.com>
2023-07-06 02:04:49 -0600

    add: agregando mas info de los links

--- webScrapper.py
+++ webScrapper.py
@@ -1,15 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = input('Ingrese la url del sitio: ')
-response = requests.get(url)
+url = input('Ingrese la URL del sitio: ')
 
-if response.status_code == 200:
+try:
+    response = requests.get(url)
+    response.raise_for_status()
 
     soup = BeautifulSoup(response.content, 'html.parser')
     links = soup.find_all('a')
-    for link in links:
-        print(link.get('href'))
 
-else:
-    print('No se pudo acceder al sitio web:', response.status_code)
+    print('Enlaces encontrados \n')
+
+    for link in links:
+        href = link.get('href')
+        text = link.text.strip()
+        print(f'Enlace: {href}')
+        print(f'Texto: {text}')
+        # Imprime otros atributos relevantes si es necesario
+
+except requests.HTTPError as e:
+    print('Error al acceder al sitio web:', e)
+except requests.RequestException as e:
+    print('Error de solicitud:', e)
+except Exception as e:
+    print('Error inesperado:', e)
